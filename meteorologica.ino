1c7b4ade11758e5afcb05ae931096e0c783bf73b
Bromaeg <a01709653@tec.mx>
2022-10-21 18:52:28 -0500

    Add the fotoresistor module code for its light sensing.

--- meteorologica.ino
+++ meteorologica.ino
@@ -1,13 +1,20 @@
 #include <DHT.h> #include <DHT U.h>
 DHT dht (2, DHT11);
 float temp, hume;
+#define Photoresistor A0
 void setup()
- {
+{
 Serial.begin (9600);
-dht.begin ();}
+dht.begin ();
+
+}
 void loop() {
+delay(2000);
 hume = dht.readHumidity ();
 temp = dht.readTemperature () ;
-Serial.println ("Temperatura: " + String (temp) + " Humedad: " + String (hume) );
-delay(1200);
+int a_value = analogRead(Photoresistor);
+int brillo = map(a_value,0,1000,0,100);
+Serial.println ("Temperatura: " + String (temp) + " Humedad: " + String (hume) +" Nivel de Brillo: " + int(brillo));
+
 }
+
